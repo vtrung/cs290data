fc1f589f1c5e67b6ad9ae0d4d43b4eb36031d0c8
Ving Trung <vtrung>
2015-12-06 19:40:47 -0800

    remove test codes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3001);
+app.set('port', 3000);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -87,7 +87,7 @@ app.get('/table',function(req,res,next){
       next(err);
       return;
     }
-    context.results = JSON.stringify(rows);
+    //context.results = JSON.stringify(rows);
     res.render('table', context);
   });
 });
